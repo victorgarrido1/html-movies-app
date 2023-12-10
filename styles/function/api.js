330ecd7106d9a4140e055e6e34e5485c614f4069
victorgarrido1 <vcgarr94@gmail.com>
2023-12-09 21:54:04 -0700

    new url

--- styles/function/api.js
+++ styles/function/api.js
@@ -1,31 +1,16 @@
-const url =
-  "https://streaming-availability.p.rapidapi.com/search/title?title=%3CREQUIRED%3E&country=us&show_type=all&output_language=en";
+const url = 'https://moviesdatabase.p.rapidapi.com/titles/search/keyword/%7Bkeyword%7D';
 const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "3065ca2efamsh5511818a494e23fp1cbb2cjsna2ae6a53946f",
-    "X-RapidAPI-Host": "streaming-availability.p.rapidapi.com",
-  },
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3065ca2efamsh5511818a494e23fp1cbb2cjsna2ae6a53946f',
+		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+	}
 };
 
-async function getMovie() {
-  const resp = await fetch(url);
-  const respData = await resp.json();
-
-  console.log(respData)
-
-  respData.r
-}// try {
-//   const response = await fetch(url, options);
-//   const result = await response.text();
-//   console.log(result);
-// } catch (error) {
-//   console.error(error);
-// }
-
-async function getMovie() {
-  const resp = await fetch(url);
-  const respData = await resp.json();
-
-  console.log(respData)
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
 }
