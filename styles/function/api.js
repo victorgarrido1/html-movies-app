8f6830a8d0b0ab8471c2654de5df679019b79295
victorgarrido1 <vcgarr94@gmail.com>
2023-12-10 14:54:24 -0700

    fixing api

--- styles/function/api.js
+++ styles/function/api.js
@@ -1,16 +1,43 @@
-const url = 'https://moviesdatabase.p.rapidapi.com/titles/search/keyword/%7Bkeyword%7D';
+async function fetchApi(url, options) {
+  
+  try {
+
+    // Fetch API
+    const result = await fetch(url, options);
+
+    // Convert Result to JSON
+    const resultJson = await result.json();
+
+    // Return Result
+    return result;
+
+  } catch (error) {
+    
+    // Log Error to console
+    console.log(`Error fetching API: ${JSON.stringify(error)}`);
+
+  }
+}
+
+// Get Movie Title from HTML Field
+let title = document.getElementById('title').value;
+
+// Clean Movie Title
+title = title.trim();
+
+// Encode Movie Title for URL
+encodeURIComponent(title);
+
+// Create URL for Fetch API
+let url = `https://streaming-availability.p.rapidapi.com/search/title?title=${title}&country=us&show_type=all&output_language=en`;
+
+// Options for Fetch API
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '3065ca2efamsh5511818a494e23fp1cbb2cjsna2ae6a53946f',
-		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-	}
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '3065ca2efamsh5511818a494e23fp1cbb2cjsna2ae6a53946f',
+    'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+  },
 };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+fetchApi(url, options);
